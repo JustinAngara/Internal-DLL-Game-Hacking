f58f49a1fbc7a418711695277ee66c369641e3c8
Justin Angara <justin.angara@gmail.com>
2026-07-28 13:52:49 -0500

    adding more anti dbg prac

---
+++ AntiCheatTester/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+// anti cheat, anti dbg, obfuscation tester, random stuff
+int main()
+{
+	std::cout << "still in working\n";
+	printf("Press enter to exit.\n");
+	std::cin.get();
+
+	return 0;
+}

c70cc58f95ed3f921e2541990d5bef0f7287474b
Justin Angara <justin.angara@gmail.com>
2026-09-09 21:07:41 -0500

    more test

--- AntiCheatTester/SDK/Test/Test.h
+++ AntiCheatTester/SDK/Test/Test.h
@@ -1,7 +1,28 @@
 #pragma once
+#include <string>
+#include "../../Vars.h"
+
+
+using TestFunc = void(*)();   
+
+class Test
+{
+public:
+	void Run(); // make sure to run isCalled, then m_func();
+private:
+	std::string m_name;
+	bool        isCalled;
+	TestFunc    m_func;
+	bool        isSuccessful;
+};
+
 // enable tester
 namespace Tester
 {
-	bool g_IsTestingOn = false;
+	bool IsTestingOn    = true;
+
+	bool IsGUIDisplayOn = true;
+	bool EnableLogging  = true;
+
 	
 }
